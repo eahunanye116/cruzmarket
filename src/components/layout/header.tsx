78dc0bdff4066aaa2b42e7e6a66db81d18c4a1b7
Emmanuel Solomon <eahunanya116@gmail.com>
2025-12-15 00:38:19 +0000

    Cool the image should also display in the header too

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -49,7 +49,7 @@ export function Header() {
   ];
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b-2 border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b-2 border-border/40 bg-background/0 backdrop-blur supports-[backdrop-filter]:bg-background/0">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
